ec52eebd87989537f99b7cf7289e62b5ed671149
straboy <straboy.lin@gmail.com>
2017-12-21 12:04:40 -0800

    cleanup code

--- server/binary.js
+++ server/binary.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
+const path = require('path');
 
 module.exports = {
   get: (req, res) => {
-    console.log('its here ', __dirname + `/../${req.params.Id}.mp4`);
+    console.log('its here ', path.join(__dirname, `/../${req.params.Id}.mp4`));
     res.writeHead(200, { ContenType: 'video/mp4' });
-    fs.createReadStream(__dirname + `/../assets/${req.params.Id}.mp4`).pipe(res);
+    fs.createReadStream(path.join(__dirname, `/../${req.params.Id}.mp4`)).pipe(res);
   },
 };
