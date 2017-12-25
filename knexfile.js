fb11b608255942218c1e830eb933c03247a6868c
straboy <straboy.lin@gmail.com>
2017-12-24 20:11:30 -0800

    config production env knex

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,26 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
+      host: 'db',
+      user: 'jinxuanlin',
+      password: '',
+      database: 'youtube',
+      pool: {
+        min: 1,
+        max: 75,
+      },
+    },
+    migrations: {
+      directory: path.join(__dirname, '/db/migrations'),
+    },
+    seeds: {
+      directory: path.join(__dirname, '/db/seeds'),
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      host: 'db',
       user: 'jinxuanlin',
       password: '',
       database: 'youtube',
